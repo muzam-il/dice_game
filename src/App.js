6af035ff5841a6f72b024eb9bed859d1293772ab
Muhamed Musammil P <p.musammil98@gmail.com>
2019-08-06 22:34:17 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="col-2 offset-5 text-center ">
+      <h2>Dice Game</h2>
+    </div>
+  );
+}
+
+export default App;
